7be8f343109d8c4a27a1422aab114eb2f0f95b0c
VenomXD21 <om.gawale24@vit.edu>
2026-04-20 17:38:12 +0530

    Clean up root directory, removing legacy files

--- routes/checkin.js
+++
@@ -1,9 +0,0 @@
-// routes/checkin.js
-const express = require("express");
-const router = express.Router();
-const { checkIn, getUserStats } = require("../controllers/checkinController");
-
-router.post("/",              checkIn);      // POST /api/checkin
-router.get("/stats/:userId",  getUserStats); // GET  /api/checkin/stats/:userId
-
-module.exports = router;
